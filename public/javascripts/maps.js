7f6b2ad8acec44d51a56b3b15493663d29303abf
“Lorie <lorie.rocher@gmail.com>
2020-09-16 15:02:17 +0200

    product details and overvew

--- public/javascripts/maps.js
+++ public/javascripts/maps.js
@@ -1,74 +1,78 @@
-mapboxgl.accessToken ="pk.eyJ1Ijoic29waGlhZnJlZGVyaWtlIiwiYSI6ImNrZjNycHZ4ZjA0NWUyenM5eGV2cjNucTQifQ.ymS-cOe15o4AeRuok1nJnQ";
+mapboxgl.accessToken = "pk.eyJ1Ijoic29waGlhZnJlZGVyaWtlIiwiYSI6ImNrZjNycHZ4ZjA0NWUyenM5eGV2cjNucTQifQ.ymS-cOe15o4AeRuok1nJnQ";
 
 const map = new mapboxgl.Map({
   container: 'map',
   style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
-  center: [13.404954,52.520008], // starting position [lng, lat]
+  center: [13.404954, 52.520008], // starting position [lng, lat]
   zoom: 9 // starting zoom
-  });
+});
 
-  const popup = new mapboxgl.Popup();
+const popup = new mapboxgl.Popup();
 
-  let marker = new mapboxgl.Marker({
+let marker = new mapboxgl.Marker({
     draggable: true
-    })
-    .setLngLat([13.405, 52.52])
-    .addTo(map);
-     
-    function onDragEnd() {
-    
-    let lngLat = marker.getLngLat();
-    coordinates.style.display = 'block';
-    coordinates.innerHTML =
+  })
+  .setLngLat([13.405, 52.52])
+  .addTo(map);
+
+function onDragEnd() {
+
+  let lngLat = marker.getLngLat();
+  coordinates.style.display = 'block';
+  coordinates.innerHTML =
     'Longitude: ' + lngLat.lng + '<br />Latitude: ' + lngLat.lat;
-    }
-     
-    marker.on("dragend", (data) => {
-      console.log("hello? Is it data?", Object.values(data.target.getLngLat()));
-      popup.addTo(map);
-      popup.setLngLat(Object.values(data.target.getLngLat()));
-      popup.setMaxWidth("400px");
-      popup.setHTML(
+}
+
+marker.on("dragend", (data) => {
+  console.log("hello? Is it data?", Object.values(data.target.getLngLat()));
+  popup.addTo(map);
+  popup.setLngLat(Object.values(data.target.getLngLat()));
+  popup.setMaxWidth("400px");
+  popup.setHTML(
 
 
-        `<form action="/add-products" method="POST" id="form" enctype="multipart/form-data">
+    `<form class="form-container" id="map-add-product" action="/add-products" method="POST" id="form" enctype="multipart/form-data">
 
-        <label for="title">Title of your product</label>
-        <input name="title" type="text" id="title">
+        <label class="label-add" for="title">Product name:</label>
+        <input class="input" name="title" type="text" id="title">
       
-       <label for="brand">Brand:</label>
-        <input name="brand" type="text" id="brand">
+       <label class="label-add" for="brand">Brand:</label>
+        <input class="input" name="brand" type="text" id="brand">
       
-        <label for="start">When does your product expire?</label>
-        <input type="date" id="expiryDate" name="expiryDate" value="2020-09-01">
+        <label class="label-add" for="start">Expiry Date:</label>
+        <input class="input" type="date" id="expiryDate" name="expiryDate" value="2020-09-01">
         
       
-        <label for="description">Describe your product</label>
-        <input name="description" type="text" id="description">
+        <label class="label-add" for="description">Description:</label>
+        <input class="input" name="description" type="text" id="description">
       
-        <label for="image">add an image of your product </label>
-        <input type="file" name="image" id="image">
-      
-       <label>Select a category</label>
+        <div class="input-container picture">
+        <label class="label-add" for="image">Picture:</label>
+        <input class="input" type="file" name="image" id="image">
+        </div>
+        <div class="input-container picture">
+       <label class="label-add" >Category:</label>
                    <select name="category" id = "myList">
-                     <option value = "fruitsandvegetables">fruitsandvegetables</option>
-                     <option value = "alreadycooked">alreadycooked</option>
-                     <option value = "condiments">condiments</option>
-                     <option value = "dairy">dairy</option>
-                     <option value = "meatandfish">meatandfish</option>
-                     <option value = "veggieproducts">veggieproducts</option>
-                     <option value = "drygoods">drygoods</option>
-                     <option value = "backeryproducts">backeryproducts</option>
-                     <option value = "drinks">drinks</option>
+                     <option value = "fruitsandvegetables">Fruits & Veggies</option>
+                     <option value = "alreadycooked">Already Cooked</option>
+                     <option value = "condiments">Condiments</option>
+                     <option value = "dairy">Dairy</option>
+                     <option value = "meatandfish">Meat & fish</option>
+                     <option value = "veggieproducts">Veggie Products</option>
+                     <option value = "drygoods">Dry Goods</option>
+                     <option value = "backeryproducts">Backery Products</option>
+                     <option value = "drinks">Drinks</option>
                    </select>
+                   </div>
 
         <input style="display: none" type="text" name="coordinates" value=${Object.values(data.target.getLngLat())}>
 
       
-        <button type="submit">Add your product</button>
+        <button class="btn btn-add-product " type="submit">Add your product!</button>
       </form>`
-      );
-    });
+   
+  );
+});
 
 //const map = new mapboxgl.Map({
 //   container: "map",
--- routes/products.js
+++ routes/products.js
@@ -8,5 +8,16 @@ router.get("/product/edit", (req, res, next) => {
   res.render("products/edit-product");
 });
 
+router.get("/products-overview", (req, res) => {
+  Product.find()
+     .then(product => {
+      console.log(product)
+      res.render("products/products-overview", {product: product})
+     })
+     .catch(err => {
+       next(err);
+     });
+ });
+
 
 module.exports = router;
